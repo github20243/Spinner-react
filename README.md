c37fa5fb007f6db87cbb0bd5284759c4910d4aee
nurs_996 <98372@gamil.com>
2024-10-02 18:48:34 +0600

    сделал описание спинеров с readme.md

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Спиннеры
+
+В проекте реализованы несколько спиннеров для отображения состояния загрузки:
+
+1. **Spinner**: Использует компонент `ScaleLoader` из библиотеки `react-spinners`. Он отображает анимацию загрузки с текстом "Загрузка...". Фон полупрозрачный, а контент спиннера центрирован.
+
+2. **Spinner2**: Использует компонент `DotLoader` из `react-spinners`. Этот спиннер также отображает текст "Загрузка..." и имеет стиль с тенью и эффектом увеличения при наведении.
+
+3. **Spinner3**: Использует компонент `RingLoader` из `react-spinners`. Он имеет аналогичный стиль, как и предыдущие спиннеры, с текстом "Загрузка..." и полупрозрачным фоном.
+
+Каждый спиннер принимает пропс `isLoading`, который управляет его отображением. Спиннеры могут быть использованы для улучшения пользовательского опыта при загрузке данных.
--- src/components/Spinner2.jsx
+++ src/components/Spinner2.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled } from '@mui/system';
-import { DotLoader } from 'react-spinners'; // Импортируем DotLoader
+import { DotLoader } from 'react-spinners'; 
 
 export const Spinner2 = ({ isLoading }) => {
   if (!isLoading) return null;
@@ -8,7 +8,7 @@ export const Spinner2 = ({ isLoading }) => {
   return (
     <SpinnerOverlay>
       <SpinnerContent>
-        <DotLoader size={60} color="#11e4e4" /> {/* Используем DotLoader */}
+        <DotLoader size={60} color="#11e4e4" /> 
         <SpinnerText>Загрузка...</SpinnerText>
       </SpinnerContent>
     </SpinnerOverlay>
@@ -36,20 +36,20 @@ const SpinnerContent = styled('div')(({}) => ({
   height: '200px',
   backgroundColor: 'white',
   justifyContent: 'center',
-  borderRadius: '12px', // Увеличен радиус скругления
-  boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)', // Добавлена тень
-  transform: 'scale(1.05)', // Легкое увеличение
-  transition: 'transform 0.3s ease', // Плавный переход
+  borderRadius: '12px',
+  boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)', 
+  transform: 'scale(1.05)', 
+  transition: 'transform 0.3s ease',
   '&:hover': {
-    transform: 'scale(1.1)', // Увеличение при наведении
+    transform: 'scale(1.1)',
   },
 }));
 
 const SpinnerText = styled('div')(({}) => ({
   marginTop: '20px',
   color: '#11e4e4',
-  fontSize: '20px', // Увеличен размер шрифта
+  fontSize: '20px', 
   fontWeight: 'bold',
   fontFamily: 'monospace',
-  textShadow: '1px 1px 2px rgba(0, 0, 0, 0.3)', // Добавлена тень к тексту
+  textShadow: '1px 1px 2px rgba(0, 0, 0, 0.3)', 
 }));
